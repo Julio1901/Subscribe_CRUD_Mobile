cf801b863c9d68320ead535993a7e8b91a0b7525
Julio Cesar <js.cesar42@gmail.com>
2021-10-24 00:20:56 -0300

    Toolbar configured

--- app/src/main/java/com/example/myfirstcrud/MainActivity.kt
+++ app/src/main/java/com/example/myfirstcrud/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.myfirstcrud
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Configurações para a toolbar reagir de acordo com nossa navegação
+
+        //Recuperando um fragmento e passando ele para um NavHostFragment
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        //Pegando um navControler
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        app_tolbar.setupWithNavController(navController, appBarConfiguration)
+
     }
 }
